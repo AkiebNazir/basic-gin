3fa23df12aaa8b1630b1bc7d034179809e816b5c
AkeibNjasm <akieb@jetking.com>
2024-02-09 11:03:03 +0530

    added file

--- hello.go
+++ hello.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Person struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
+
+var persons []Person
 
 func HelloWorld(ctx *gin.Context) {
 	name := ctx.Param("name")
@@ -16,13 +24,42 @@ func HelloWorld(ctx *gin.Context) {
 	})
 }
 
+func AddPerson(ctx *gin.Context) {
+	var person Person
+	err := ctx.BindJSON(&person)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Error parsing body: " + err.Error(),
+		})
+	}
+	persons = append(persons, person)
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "person added successfully",
+	})
+
+}
+
+func GetAllPersons(ctx *gin.Context) {
+	byteData, err := json.Marshal(persons)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Error parsing body: " + err.Error(),
+		})
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"data": string(byteData),
+	})
+}
+
 func main() {
 	fmt.Println("Welcome to go web development framwork gin")
 
 	// initialize the gin
 	r := gin.Default()
-
+	
 	r.GET("/:name", HelloWorld)
+	r.POST("/person", AddPerson)
+	r.GET("/person", GetAllPersons)
 
 	r.Run(":5000")
 }
